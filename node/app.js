c55f9ad92f9d2884b7e58ce3c2607ef4a16e959d
dewanshrawat15 <dewanshrawat15@gmail.com>
2020-08-20 23:08:39 +0000

    Working on a simple encryption decryption app

--- node/app.js
+++ node/app.js
@@ -1,19 +1,30 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+// const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+// const http = require('http');
+// const url = require('url');
+// const hostname = '0.0.0.0';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+// const server = http.createServer((req, res) => {
+//   const queryObject = url.parse(req.url,true).query;
+//   if (!queryObject['name']) {
+//     res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+//   } else {
+//     res.end(say(queryObject['name']) + '\n');
+//   }
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const { generate_key_pair, encrypt, decrypt } = require('../pkg/ssvm_nodejs_starter_lib.js');
+
+var kp = JSON.parse(generate_key_pair(2048));
+var public_key = kp['rsa_public_key'];
+var private_key = kp['rsa_private_key'];
+
+var msg = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+var enc_data = encrypt(JSON.stringify(public_key), encoder.encode(msg));
+var dec_data = decrypt(JSON.stringify(private_key), enc_data);
+console.log(decoder.decode(dec_data));
